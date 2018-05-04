50daf285f024241a74246966b55d4518bbcf12cd
Jonas van Nijnatten <jonasvannijnatten@gmail.com>
2018-05-04 18:45:03 +0200

    Added master volume for noise
    In the playNoise function the volume if the noise can be scaled

--- Functions/playNoise.m
+++ Functions/playNoise.m
@@ -1,6 +1,11 @@
 function player = playNoise(volume, t)
 if isnan(volume); volume = 0; end
 if isnan(t); t = .01; end
+
+MasterVolume = 1; % set value between 0 and 1 to scale the volume. 
+% E.G. a setting of 0.5 will set the max volume to half the normal volume
+
+volume = volume * MasterVolume;
 Fs = 4800;
 ts = round(t * Fs);
 noise = volume * rand(1, ts);
