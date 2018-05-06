4b88441507a3f6958e0f607d3e7dc7699d9a9475
Jonas van Nijnatten <jonasvannijnatten@gmail.com>
2018-05-06 18:27:42 +0200

    update initPlots
    - changed Xlimit of plots so the recording starts at t=0

--- Functions/initiatePlots.m
+++ Functions/initiatePlots.m
@@ -18,7 +18,7 @@ hold(handles.plotBand1, 'on')
 plots.band1 = plot(handles.plotBand1  ,0); plots.band1mean = plot(handles.plotBand1, 0);
 handles.plotBand1.Title.String = REC.band1name;
 hold(handles.plotBand1, 'off')
-set(handles.plotBand1, 'XLim', [REC.fbTime REC.time])
+set(handles.plotBand1, 'XLim', [0 REC.time])
 % set(handles.plotBand1, 'YLim',[-Inf Inf])
 
 % plot power of frequency band 2
@@ -26,7 +26,7 @@ hold(handles.plotBand2, 'on')
 plots.band2 = plot(handles.plotBand2 ,0); plots.band2mean = plot(handles.plotBand2, 0);
 handles.plotBand2.Title.String = REC.band2name;
 hold(handles.plotBand2, 'off')
-set(handles.plotBand2, 'XLim', [REC.fbTime REC.time])
+set(handles.plotBand2, 'XLim', [0 REC.time])
 % set(handles.plotBand2, 'YLim',[-Inf Inf])
 
 % plot ratio of frequeny band powers
@@ -34,11 +34,11 @@ hold(handles.plotBandRatio, 'on')
 plots.ratio = plot(handles.plotBandRatio, 0); plots.ratiomean = plot(handles.plotBandRatio, 0);
 handles.plotBandRatio.Title.String = [REC.band1name '/' REC.band2name ' ratio' ];
 hold(handles.plotBandRatio, 'off')
-set(handles.plotBandRatio, 'XLim', [REC.fbTime REC.time])
+set(handles.plotBandRatio, 'XLim', [0 REC.time])
 % set(handles.plotBandRatio, 'YLim',[-Inf Inf])
 
 % plot level of the output signal
 plots.output = plot(handles.outputSignal, 0);
 handles.outputSignal.Title.String = 'Output Signal';
-set(handles.outputSignal, 'XLim', [REC.fbTime REC.time])
+set(handles.outputSignal, 'XLim', [0 REC.time])
 % set(handles.outputSignal, 'YLim',[-Inf Inf])
